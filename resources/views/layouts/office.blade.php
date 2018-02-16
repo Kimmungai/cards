a209813065fda34e344187892e75352757da5a17
kimprojects <kimpita9@gmail.com>
2018-02-16 22:37:24 +0900

    Table taking shape

--- resources/views/layouts/office.blade.php
+++ resources/views/layouts/office.blade.php
@@ -4,12 +4,13 @@
     <meta charset="utf-8">
     <title>Virtual office dashboard</title>
     <link  href="/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body>
     <div class="ribbon"></div>
     <div class="container">
       <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-3">
           <div id="mySidenav" class="sidenav">
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
             <a href="#">About</a>
@@ -17,16 +18,20 @@
             <a href="#">Clients</a>
             <a href="#">Contact</a>
           </div>
-          <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+          <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
         </div>
-        <div id="main" class="col-md-6">
+        <div id="main" class="col-md-7">
           @yield('desk')
         </div>
         <div class="col-md-2">
-          <aside class="border">
-            <div class="avatar">
-              <img class="img-responsive rounded-circle" src="/img/profile.jpg">
-            </div>
+          <aside class="border profile">
+            <a href="#"><div class="avatar"></div></a>
+            <h2><a href="#">Peter Njui</a></h2>
+            <ul>
+              <li><a href="#">profile</a></li>
+              <li><a href="#">support</a></li>
+              <li class="text-muted"><a href="#">logout</a></li>
+            </ul>
           </aside>
         </div>
       </div>
--- resources/views/virtual_office.blade.php
+++ resources/views/virtual_office.blade.php
@@ -1,4 +1,26 @@
 @extends('layouts.office')
 @section('desk')
+<section class="working-area">
   <h1>Cards Shelf</h1>
+  <article class="card">
+    sd
+  </article>
+</section>
+<section class="border desk">
+  <h2>Desk</h2>
+  <ul>
+    <li>
+      <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+    </li>
+    <li>
+      <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+    </li>
+    <li>
+      <a href="#"><i class="fa fa-globe" aria-hidden="true"></i></a>
+    </li>
+    <li>
+      <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+    </li>
+  </ul>
+</section>
 @endsection
