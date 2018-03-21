980be1a4baba9cb7a183287546eac106cdeaf758
kimmungai <kimpita9@gmail.com>
2018-03-21 15:33:05 +0900

    cards initial commit

--- resources/views/layouts/angular-variables.blade.php
+++ resources/views/layouts/angular-variables.blade.php
@@ -10,114 +10,12 @@ var app = angular.module('App', []);
     $scope.website= "http://localhost:8000/new";
     $scope.bar_code_url= "http://localhost:8000/new";
     //display variables
-    $scope.active_shelf = 2;
-    $scope.shelf_items = 2;
+    $scope.active_shelf = 1;
+    $scope.shelf_items = 1;
     $scope.active_card_shelf_item=1;
     $scope.active_cv_shelf_item=1;
     $scope.active_letter_shelf_item=1;
     $scope.active_website_shelf_item=1;
-    //cv content
-    $scope.cv_objective="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.";
-      //Experience
-      $scope.cv_experience_title_1="Computer Programmer";
-      $scope.cv_company_1="Excia East Africa Ltd";
-      $scope.cv_company_address_1="Nairobi, Kenya -";
-      $scope.cv_experience_start_date_1="Jan-2017 -";
-      $scope.cv_experience_end_date_1="Feb-2018";
-      $scope.cv_experience_item1_1="-The standard Lorem Ipsum passage, used since the 1500";
-      $scope.cv_experience_item2_1="-Section 1.10.33 of de 'Finibus Bonorum et Malorum', written by Cicero in 45 BC";
-      $scope.cv_experience_item3_1="-1914 translation by H. Rackham";
-
-      $scope.cv_experience_title_2="Musician";
-      $scope.cv_company_2="Tpf ";
-      $scope.cv_company_address_2="Nairobi, Kenya -";
-      $scope.cv_experience_start_date_2="Jan-2015 -";
-      $scope.cv_experience_end_date_2="Feb-2019";
-      $scope.cv_experience_item1_2="-The standard Lorem Ipsum passage, used since the 1500";
-      $scope.cv_experience_item2_2="-Section 1.10.33 of de 'Finibus Bonorum et Malorum', written by Cicero in 45 BC";
-      $scope.cv_experience_item3_2="-1914 translation by H. Rackham";
-      $scope.cv_experience_2=0;
-
-      $scope.cv_experience_title_3="Mukami";
-      $scope.cv_company_3="Excia East Africa Ltd ";
-      $scope.cv_company_address_3="Nairobi, Kenya -";
-      $scope.cv_experience_start_date_3="Jan-2017 -";
-      $scope.cv_experience_end_date_3="Feb-2018";
-      $scope.cv_experience_item1_3="-The standard Lorem Ipsum passage, used since the 1500";
-      $scope.cv_experience_item2_3="-Section 1.10.33 of de 'Finibus Bonorum et Malorum', written by Cicero in 45 BC";
-      $scope.cv_experience_item3_3="-1914 translation by H. Rackham";
-      $scope.cv_experience_3=0;
-
-      $scope.cv_experience_title_4="mkimbizi";
-      $scope.cv_company_4="Excia East Africa Ltd ";
-      $scope.cv_company_address_4="Nairobi, Kenya -";
-      $scope.cv_experience_start_date_4="Jan-2017 -";
-      $scope.cv_experience_end_date_4="Feb-2018";
-      $scope.cv_experience_item1_4="-The standard Lorem Ipsum passage, used since the 1500";
-      $scope.cv_experience_item2_4="-Section 1.10.33 of de 'Finibus Bonorum et Malorum', written by Cicero in 45 BC";
-      $scope.cv_experience_item3_4="-1914 translation by H. Rackham";
-      $scope.cv_experience_4=0;
-
-    $scope.cv_education_title="BSc. Information Technology";
-    $scope.cv_education_univ="Dedan Kimathi university";
-    //letter content
-    $scope.letter_date = new Date();
-    $scope.letter_to = "John Makasi";
-    $scope.letter_to_org = "Nyeri waters and sewerage company";
-    $scope.letter_to_add = "P.O BOX 86-20107 NYERI";
-    $scope.letter_heading = "Re: Customer Service Representative Opening (Ref. ID: CS300-Denver)";
-    $scope.letter_salutation = "Dear Ms. Nogo";
-    $scope.letter_body = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-    $scope.letter_sign_off = "sincerely";
-    //****website content*********//
-    $scope.profession = "Programmer";
-    $scope.location = "Njoro";
-    $scope.main_pic = "img/profile.png";
-    $scope.facebook_link = "img/profile.png";
-    $scope.twitter_link = "img/profile.png";
-    $scope.google_plus_link = "img/profile.png";
-    $scope.linkedin_link = "img/profile.png";
-    $scope.git_hub_link = "img/profile.png";
-    $scope.tag_line_1 = "Hello, I'm Me";
-    $scope.tag_line_2 = "A kondoo Based in here";
-    //about section
-    $scope.about_title = "About Mine";
-    $scope.about_story_title = "A little brif ABOUT Me";
-    $scope.about_story_title = "A little brif ABOUT Me";
-    $scope.about_story = "Ultricies nisi voluptatem, illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo nemo enim ipsam voluptatem. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo tempor dignissim at. e vitae dicta sunt explicabo nemo enim ipsam voluptatem. ipsum dolor sit amet, consectetur adipisicing elit.<br><br>sed do eiusmod temporincididunt ut labore et dolore magna aliqua. ab illo tempor dignissim at. e vitae dicta sunt explicabo nemo enim ipsa voluptatem.";
-    $scope.resume_downloadable = 1;
-    $scope.skill_title = "SKILLS";
-    $scope.skill_1 = "Milking goats";
-    $scope.skill_1_level = 90;
-    $scope.skill_2 = "yapping";
-    $scope.skill_2_level = 19;
-    $scope.skill_3 = "Eating";
-    $scope.skill_3_level = 29;
-    $scope.skill_4 = "Making hay";
-    $scope.skill_4_level = 09;
-    //speciality areas
-    $scope.speciality_title = "What i do";
-
-    $scope.speciality_1 = "sijui";
-    $scope.speciality_1_icon = "fa-mobile";
-    $scope.speciality_1_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text";
-
-    $scope.speciality_2 = "UX / UI design";
-    $scope.speciality_2_icon = "fa-book";
-    $scope.speciality_2_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text";
-
-    $scope.speciality_3 = "Machine learning";
-    $scope.speciality_3_icon = "fa-desktop";
-    $scope.speciality_3_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text";;
-
-    $scope.speciality_4 = "Target";
-    $scope.speciality_4_icon = "fa-envelope";
-    $scope.speciality_4_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text";
-
-    //contact form section
-    $scope.contact_section_title = "Drop me a message";
-    $scope.contact_receiving_email = "nyau@nyau.com";
-    $scope.contact_auto_reply_message = "dfgd";
 
     //Edit items variables
     $scope.edit_card = 0;
--- resources/views/layouts/v-office.blade.php
+++ resources/views/layouts/v-office.blade.php
@@ -15,40 +15,27 @@
     <div  class="container"><!--start of app container-->
       <div class="row"><!--start of header section-->
         <div class="col-md-12 no-padding">
-          <header ng-cloak>
-            <nav class="navbar navbar-toggleable-md navbar-light bg-inverse">
-              <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon text-white"></span>
-              </button>
-              <a class="navbar-brand text-warning text-muted pr5" href="#">Digital PA</a>
-
-              <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav mr-auto mt-2 mt-md-0">
-                  <li class="nav-item">
-                    <a class="nav-link text-white active" href="#">Home <span class="sr-only">(current)</span></a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link text-white" href="#">help/support</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link text-white" href="#">settings</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      @{{first_name}} @{{second_name}}
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <a class="dropdown-item" href="#"><i class="fa fa-user"></i> Account</a>
-                      <a class="dropdown-item" href="#"><i class="fa fa-credit-card"></i> Membership</a>
-                      <a class="dropdown-item" href="#"><i class="fa fa-unlink"></i> Logout</a>
-                    </div>
-                  </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                  <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                </form>
+          <header class="bg-dark" ng-cloak>
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-8 affix">
+                  <h1 class="text-warning"><a href="/">BiznessKit</a></h1>
+                </div>
+                <div class="col-sm-4 affix">
+                    <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          <span class="fa fa-user"></span> @{{first_name}} @{{second_name}}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                          <a class="dropdown-item" href="#"><i class="fa fa-user"></i> Account</a>
+                          <a class="dropdown-item" href="#"><i class="fa fa-unlink"></i> Logout</a>
+                        </div>
+                      </li>
+                    </ul>
+                </div>
               </div>
-            </nav>
+            </div>
           </header>
         </div>
       </div><!--end of header section-->
@@ -59,14 +46,14 @@
             <ul class="shelf-icons">
               <li ng-class="active_shelf==1 ? 'active' : 'inactive'" ng-click="active_shelf=1;shelf_items=1"><a href="#"><i class="fa fa-book"></i></a></li>
               <li ng-class="active_shelf==2 ? 'active' : 'inactive'" ng-click="active_shelf=2;shelf_items=2"><a href="#"><i class="fa fa-file"></i></a></li>
-              <li ng-class="active_shelf==3 ? 'active' : 'inactive'" ng-click="active_shelf=3;shelf_items=3"><a href="#"><i class="fa fa-envelope"></i></a></li>
+              <li ng-class="active_shelf==3 ? 'active' : 'inactive'" ng-click="active_shelf=3;shelf_items=3"><a href="#"><i class="fa fa-share-alt"></i></a></li>
               <li ng-class="active_shelf==4 ? 'active' : 'inactive'" ng-click="active_shelf=4;shelf_items=4"><a href="#"><i class="fa fa-globe"></i></a></li>              <!--<li><a class="text-danger" href="#"><i class="fa fa-plus-circle"></i></a></li>-->
             </ul>
             <ul class="shelves">
-              <li ng-click="active_shelf=1;shelf_items=1"><a href="#">cards</a></li>
-              <li ng-click="active_shelf=2;shelf_items=2"><a href="#">cvs</a></li>
-              <li ng-click="active_shelf=3;shelf_items=3"><a href="#"> letters</a></li>
-              <li ng-click="active_shelf=4;shelf_items=4"><a href="#"> websites</a></li>
+              <li ng-click="active_shelf=1;shelf_items=1"><a href="#">My cards</a></li>
+              <li ng-click="active_shelf=2;shelf_items=2"><a href="#">Templates</a></li>
+              <li ng-click="active_shelf=3;shelf_items=3"><a href="#"> Share</a></li>
+              <li ng-click="active_shelf=4;shelf_items=4"><a href="#"> Help</a></li>
             </ul>
             <section class="desk" ng-show="shelf_items==1">
               <h2><i class="fa fa-folder-open" aria-hidden="true"></i> Cards <span class="close" ng-click="shelf_items=0; active_shelf=0"><i class="fa fa-close" aria-hidden="true"></i></span></h2>
@@ -95,49 +82,6 @@
                 </li>
               </ul>
             </section><!--end cards shelf items-->
-            <section class="desk" ng-show="shelf_items==2">
-              <h2><i class="fa fa-folder-open" aria-hidden="true"></i> CVs <span class="close" ng-click="shelf_items=0; active_shelf=0"><i class="fa fa-close" aria-hidden="true"></i></span></h2>
-              <ul>
-                <li ng-class="active_cv_shelf_item==1 ? 'active' : 'inactive'" ng-click="active_cv_shelf_item=1" class="A">
-                  <a href="#"><i class="fa fa-user-secret" aria-hidden="true"></i></a>
-                </li>
-                <li ng-class="active_cv_shelf_item==2 ? 'active' : 'inactive'" ng-click="active_cv_shelf_item=2" class="B">
-                  <a href="#"><i class="fa fa-plane" aria-hidden="true"></i></a>
-                </li>
-              </ul>
-              <ul class="mb3">
-                <li ng-class="active_cv_shelf_item==3 ? 'active' : 'inactive'" ng-click="active_cv_shelf_item=3" class="C">
-                  <a href="#"><i class="fa fa-image" aria-hidden="true"></i></a>
-                </li>
-              </ul>
-            </section><!--end cvs shelf items-->
-            <section class="desk" ng-show="shelf_items==3">
-              <h2><i class="fa fa-folder-open" aria-hidden="true"></i> Letters <span class="close" ng-click="shelf_items=0; active_shelf=0"><i class="fa fa-close" aria-hidden="true"></i></span></h2>
-              <ul>
-                <li ng-class="active_letter_shelf_item==1 ? 'active' : 'inactive'" ng-click="active_letter_shelf_item=1" class="A">
-                  <a href="#"><i class="fa fa-user-secret" aria-hidden="true"></i></a>
-                </li>
-                <li ng-class="active_letter_shelf_item==2 ? 'active' : 'inactive'" ng-click="active_letter_shelf_item=2" class="B">
-                  <a href="#"><i class="fa fa-plane" aria-hidden="true"></i></a>
-                </li>
-              </ul>
-              <ul class="mb3">
-                <li ng-class="active_letter_shelf_item==3 ? 'active' : 'inactive'" ng-click="active_letter_shelf_item=3" class="C">
-                  <a href="#"><i class="fa fa-image" aria-hidden="true"></i></a>
-                </li>
-                <li ng-class="active_letter_shelf_item==4 ? 'active' : 'inactive'" ng-click="active_letter_shelf_item=4" class="D">
-                  <a href="#"><i class="fa fa-link" aria-hidden="true"></i></a>
-                </li>
-              </ul>
-            </section><!--end letters shelf items-->
-            <section class="desk" ng-show="shelf_items==4">
-              <h2><i class="fa fa-folder-open" aria-hidden="true"></i> Websites <span class="close" ng-click="shelf_items=0; active_shelf=0"><i class="fa fa-close" aria-hidden="true"></i></span></h2>
-              <ul class="mb3">
-                <li ng-class="active_website_shelf_item==1 ? 'active' : 'inactive'" ng-click="active_website_shelf_item=1" class="A">
-                  <a href="#"><i class="fa fa-user-secret" aria-hidden="true"></i></a>
-                </li>
-              </ul>
-            </section><!--end websites shelf items-->
           </aside>
         </div><!--end of aside section-->
 
@@ -232,76 +176,6 @@
             <div class="form-group">
               <a class="btn btn-success" href="#" ng-click="edit_cv_basic=0"><span class="fa fa-thumbs-up"></span> Done</a>
               <a class="btn btn-info" href="#" ng-click="edit_cv_basic=0;edit_cv_experience=0;edit_cv_objective=1">Next <span class="fa fa-chevron-right"></span></a>
-            </div>
-         </form>
-        </div>
-      </div>
-    </div>
-  </div><!--edit cv experience panel ends here-->
-
-  <div class="container edit-pane" ng-cloak ng-show="edit_cv_objective==1"><!--edit cv objective panel starts-->
-    <div class="container">
-      <div class="row">
-        <div class="col-md-3 no-padding edit-form-sec">
-          <span class="close" ng-click="edit_cv_objective=0"><i class="fa fa-close" aria-hidden="true"></i></span>
-          <h2><i class="fa fa-edit" aria-hidden="true"></i> CV Objective</h2>
-          <form>
-            <div class="form-group">
-              <label for="cv_objective">Objective</label>
-              <textarea type="text" class="form-control" id="cv_objective" placeholder="Your career objective" ng-model="cv_objective" rows="5"></textarea>
-            </div>
-            <div class="form-group">
-              <a class="btn btn-success" href="#" ng-click="edit_cv_objective=0"><span class="fa fa-thumbs-up"></span> Done</a>
-              <a class="btn btn-info" href="#" ng-click="edit_cv_basic=0;edit_cv_objective=0;edit_cv_experience=1">Next <span class="fa fa-chevron-right"></span></a>
-            </div>
-         </form>
-        </div>
-      </div>
-    </div>
-  </div><!--edit cv objective panel ends here-->
-
-  <div class="container edit-pane" ng-cloak ng-show="edit_cv_experience==1"><!--edit cv experience panel starts-->
-    <div class="container">
-      <div class="row">
-        <div class="col-md-3 no-padding edit-form-sec">
-          <span class="close" ng-click="edit_cv_experience=0"><i class="fa fa-close" aria-hidden="true"></i></span>
-          <h2><i class="fa fa-edit" aria-hidden="true"></i> CV Experience edit panel</h2>
-          <form>
-            <div class="form-group">
-              <label for="cv_experience_title_1">Experience 1</label>
-              <input type="text" class="form-control mb1" id="cv_experience_title_1" placeholder="Job title" ng-model="cv_experience_title_1">
-              <input type="text" class="form-control mb1" id="cv_company_1" placeholder="Company name" ng-model="cv_company_1">
-              <input type="text" class="form-control mb1" id="cv_company_address_1" placeholder="Company address" ng-model="cv_company_address_1">
-              <input type="text" class="form-control mb1" id="cv_experience_start_date_1" placeholder="Start date" ng-model="cv_experience_start_date_1">
-              <input type="text" class="form-control mb1" id="cv_experience_end_date_1" placeholder="End date" ng-model="cv_experience_end_date_1">
-              <textarea  class="form-control" id="cv_experience_item1_1" ng-model="cv_experience_item1_1" rows="5" placeholder="Statement one"></textarea>
-            </div>
-
-            <div class="form-group" ng-if="cv_experience_2 == 1">
-              <label for="cv_experience_title_2">Experience 2</label>
-              <input type="text" class="form-control mb1" id="cv_experience_title_2" placeholder="Job title" ng-model="cv_experience_title_2">
-              <input type="text" class="form-control mb1" id="cv_company_2" placeholder="Company name" ng-model="cv_company_2">
-              <input type="text" class="form-control mb1" id="cv_company_address_2" placeholder="Company address" ng-model="cv_company_address_2">
-              <input type="text" class="form-control mb1" id="cv_experience_start_date_2" placeholder="Start date" ng-model="cv_experience_start_date_2">
-              <input type="text" class="form-control mb1" id="cv_experience_end_date_2" placeholder="End date" ng-model="cv_experience_end_date_2">
-              <textarea  class="form-control" id="cv_experience_item1_2" ng-model="cv_experience_item1_2" rows="5" placeholder="Statement one"></textarea>
-            </div>
-
-            <div class="form-group" ng-if="cv_experience_3 == 1">
-              <label for="cv_experience_title_2">Experience 3</label>
-              <input type="text" class="form-control mb1" id="cv_experience_title_3" placeholder="Job title" ng-model="cv_experience_title_3">
-              <input type="text" class="form-control mb1" id="cv_company_3" placeholder="Company name" ng-model="cv_company_3">
-              <input type="text" class="form-control mb1" id="cv_company_address_3" placeholder="Company address" ng-model="cv_company_address_3">
-              <input type="text" class="form-control mb1" id="cv_experience_start_date_3" placeholder="Start date" ng-model="cv_experience_start_date_3">
-              <input type="text" class="form-control mb1" id="cv_experience_end_date_3" placeholder="End date" ng-model="cv_experience_end_date_3">
-              <textarea  class="form-control" id="cv_experience_item1_2" ng-model="cv_experience_item1_2" rows="5" placeholder="Statement one"></textarea>
-            </div>
-
-            <a href="#" ng-click="cv_experience_2 = 1" ng-if="cv_experience_2 == 1"><span class="fa fa-plus-circle"></span> Add experience</a>
-
-            <div class="form-group">
-              <a class="btn btn-success" href="#" ng-click="edit_cv_experience=0"><span class="fa fa-thumbs-up"></span> Done</a>
-              <!--<a class="btn btn-info" href="#" ng-click="edit_cv=0">Next <span class="fa fa-chevron-right"></span></a>-->
             </div>
          </form>
         </div>
